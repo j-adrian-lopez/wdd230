736600b1bf633542716adebba0134b3216f81491
j-adrian-lopez <adrj156@byui.edu>
2023-06-01 23:56:52 -0300

    Add progressive loading of images assignment

---
+++ lesson-7/scripts/imagehandling.js
@@ -0,0 +1,34 @@
+// select all data-src
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// select each image, assign the data-src content to src and remove data-src
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  };
