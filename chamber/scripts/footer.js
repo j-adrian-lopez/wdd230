b9a0ccd7cd60a4cc3dc417e162734947f89ae4ee
j-adrian-lopez <adrj156@byui.edu>
2023-06-10 19:48:01 -0400

    Added join the chamber page

--- chamber/scripts/footer.js
+++ chamber/scripts/footer.js
@@ -1,5 +1,9 @@
 //Script for index.html
 
 // Input into HTML
-document.getElementById("year").innerHTML = new Date().getFullYear();
+const day = new Date();
+document.getElementById("year").innerHTML = day.getFullYear();
 document.querySelector("#lastUpdated").textContent = `Last Modification: ${document.lastModified}`
+
+// Insert datetime in input
+document.querySelector("#date-input").value = day;
